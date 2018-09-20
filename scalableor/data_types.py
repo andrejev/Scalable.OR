80c41f10f068e38fe14da139368bbb3d9c047f70
Magnus Bieneck <magnus@bieneck.eu>
2018-09-20 09:32:36 +0200

    Added data types 'date-iso8601', 'time-24h' and 'aircraft-reg-us' for testing purposes

--- scalableor/data_types.py
+++ scalableor/data_types.py
@@ -1,6 +1,45 @@
 from scalableor.manager import DataTypeManager
 
 
+# Helper functions
+def check_date(day, month, year):
+
+    # Check year
+    year = int(year)
+    leap_year = year % 4 == 0
+
+    # Check month
+    month = int(month)
+    if month < 1 or month > 12:
+        return False
+
+    # Check day
+    day = int(day)
+    if day < 1:
+        return False
+
+    # Months with 31 days
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        if day > 31:
+            return False
+
+    # Months with 30 days
+    elif month in [4, 6, 9, 11]:
+        if day > 30:
+            return False
+
+    # February
+    else:
+        if leap_year:
+            if day > 29:
+                return False
+        else:
+            if day > 28:
+                return False
+
+    return True
+
+
 @DataTypeManager.register("ipv4")
 def ipv4(field):
 
@@ -42,40 +81,7 @@ def date_de(field):
 
     day, month, year = blocks
 
-    # Check year
-    year = int(year)
-    leap_year = year % 4 == 0
-
-    # Check month
-    month = int(month)
-    if month < 1 or month > 12:
-        return False
-
-    # Check day
-    day = int(day)
-    if day < 1:
-        return False
-
-    # Months with 31 days
-    if month in [1, 3, 5, 7, 8, 10, 12]:
-        if day > 31:
-            return False
-
-    # Months with 30 days
-    elif month in [4, 6, 9, 11]:
-        if day > 30:
-            return False
-
-    # February
-    else:
-        if leap_year:
-            if day > 29:
-                return False
-        else:
-            if day > 28:
-                return False
-
-    return True
+    return check_date(day, month, year)
 
 
 @DataTypeManager.register("credit-card")
@@ -99,3 +105,80 @@ def credit_card(field):
         sum = sum + digit
 
     return (sum % 10) == 0
+
+
+@DataTypeManager.register("date-iso8601")
+def date_iso8601(field):
+
+    # ISO-8601 Date consists of three blocks
+    blocks = field.split("-")
+
+    if len(blocks) != 3:
+        return False
+
+    # Each block must be a number
+    for block in blocks:
+        if not block.isdigit():
+            return False
+
+    year, month, day = blocks
+
+    return check_date(day, month, year)
+
+
+@DataTypeManager.register("aircraft-reg-us")
+def aircraft_reg_us(field):
+
+    # The number must be between 1 and 5 characters long (so with the "N", 2 to 6 characters)
+    if not 2 <= len(field) <= 6:
+        return False
+
+    # The first character must be an "N"
+    if field[0].lower() != "N":
+        return False
+
+    # The second character must be a digit other than zero
+    if not field[1].isdigint():
+        return False
+
+    if int(field[1]) == 0:
+        return False
+
+    # The numbers must not contain the letters I or O
+    for char in field.lower():
+        if char == "i" or char == "o":
+            return False
+
+    return True
+
+
+@DataTypeManager.register("time-24h")
+def time_24h(field):
+
+    # Length must be between 3 and 5 characters
+    if not 3 <= len(field) <= 5:
+        return False
+
+    # The time consists of two blocks, separated by a ":"
+    blocks = field.split(":")
+
+    if len(blocks) != 2:
+        return False
+
+    hours, minutes = blocks
+
+    # Hours must be digits between 0 and 23
+    if not hours.isdigit():
+        return False
+
+    if not 0 <= int(hours) <= 23:
+        return False
+
+    # Minutes must be digits between 0 and 59
+    if not minutes.isdigit():
+        return False
+
+    if not 0 <= int(minutes) <= 59:
+        return False
+
+    return True
